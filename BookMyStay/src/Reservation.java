a3771dde7982cd477de3db221e915dc6c27ebf23
Shiny <dattaa891@gmail.com>
2026-03-11 22:56:50 +0530

    Akash Datta added UC5

---
+++ BookMyStay/src/Reservation.java
@@ -0,0 +1,21 @@
+/**
+ * Represents a guest's intent to book a room.
+ * This is a data carrier for the request queue.
+ */
+public class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() { return guestName; }
+    public String getRoomType() { return roomType; }
+
+    @Override
+    public String toString() {
+        return "Request [Guest: " + guestName + " | Room: " + roomType + "]";
+    }
+}
